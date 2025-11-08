8ea176aa59cd90720cc957f34fd026d4c07087de
Tianze <hertz2000@126.com>
2025-11-08 15:17:07 +0800

    refactor: Extract embedded JS data to external JSON file

--- script.js
+++ script.js
@@ -12,61 +12,78 @@ const CHART_CONFIG = {
   }
 };
 
-// 示例数据
-const data = {
-  "entities": [
-    { "name": "美国", "scores": { "经济": 95, "教育": 90, "环境": 68, "科技": 98, "医疗": 83, "文化": 85, "安全": 75, "基础设施": 90, "生活质量": 88 } },
-    { "name": "印度", "scores": { "经济": 78, "教育": 65, "环境": 55, "科技": 82, "医疗": 60, "文化": 80, "安全": 60, "基础设施": 65, "生活质量": 58 } },
-    { "name": "德国", "scores": { "经济": 89, "教育": 93, "环境": 77, "科技": 90, "医疗": 85, "文化": 90, "安全": 83, "基础设施": 88, "生活质量": 85 } },
-    { "name": "巴西", "scores": { "经济": 68, "教育": 70, "环境": 60, "科技": 75, "医疗": 65, "文化": 75, "安全": 55, "基础设施": 70, "生活质量": 65 } },
-    { "name": "日本", "scores": { "经济": 88, "教育": 82, "环境": 70, "科技": 95, "医疗": 81, "文化": 85, "安全": 80, "基础设施": 92, "生活质量": 80 } },
-    { "name": "南非", "scores": { "经济": 63, "教育": 58, "环境": 50, "科技": 72, "医疗": 58, "文化": 70, "安全": 50, "基础设施": 60, "生活质量": 55 } },
-    { "name": "法国", "scores": { "经济": 87, "教育": 80, "环境": 75, "科技": 91, "医疗": 85, "文化": 90, "安全": 78, "基础设施": 85, "生活质量": 83 } },
-    { "name": "印度尼西亚", "scores": { "经济": 70, "教育": 60, "环境": 55, "科技": 73, "医疗": 63, "文化": 74, "安全": 60, "基础设施": 65, "生活质量": 60 } },
-    { "name": "加拿大", "scores": { "经济": 82, "教育": 92, "环境": 88, "科技": 84, "医疗": 92, "文化": 85, "安全": 85, "基础设施": 88, "生活质量": 90 } },
-    { "name": "墨西哥", "scores": { "经济": 75, "教育": 68, "环境": 58, "科技": 70, "医疗": 65, "文化": 78, "安全": 55, "基础设施": 65, "生活质量": 60 } },
-    { "name": "英国", "scores": { "经济": 90, "教育": 86, "环境": 72, "科技": 93, "医疗": 85, "文化": 88, "安全": 80, "基础设施": 85, "生活质量": 87 } },
-    { "name": "越南", "scores": { "经济": 65, "教育": 68, "环境": 55, "科技": 72, "医疗": 63, "文化": 70, "安全": 60, "基础设施": 62, "生活质量": 58 } },
-    { "name": "俄罗斯", "scores": { "经济": 76, "教育": 70, "环境": 58, "科技": 83, "医疗": 68, "文化": 75, "安全": 65, "基础设施": 70, "生活质量": 65 } },
-    { "name": "埃及", "scores": { "经济": 62, "教育": 58, "环境": 52, "科技": 63, "医疗": 60, "文化": 70, "安全": 55, "基础设施": 60, "生活质量": 55 } },
-    { "name": "阿根廷", "scores": { "经济": 72, "教育": 75, "环境": 62, "科技": 78, "医疗": 65, "文化": 80, "安全": 68, "基础设施": 70, "生活质量": 65 } }
-  ]
-};
+// 全局变量，将在数据加载后设置
+let data;
+let categories;
+let weights;
+let minScore;
+let maxScore;
+let svg;
+let x;
+let y;
+let petalLengthScale;
+let color;
 
-// 预处理数据和创建图表元素
-const categories = Object.keys(data.entities[0].scores);
-let weights = categories.map(() => 1 / categories.length);
+// 从外部JSON文件加载数据
+function loadData() {
+  d3.json("data.json")
+    .then(jsonData => {
+      data = jsonData;
+      initializeChart();
+    })
+    .catch(error => {
+      console.error("加载数据失败:", error);
+      // 提供一个简单的错误消息给用户
+      d3.select("#chart").append("p")
+        .style("color", "red")
+        .text("无法加载数据文件。请确保data.json文件存在且可访问。");
+    });
+}
 
 // 计算所有分数的极值
-const allScores = data.entities.flatMap(country => Object.values(country.scores));
-const [minScore, maxScore] = d3.extent(allScores);
+function calculateScoreExtremes() {
+  const allScores = data.entities.flatMap(country => Object.values(country.scores));
+  [minScore, maxScore] = d3.extent(allScores);
+}
 
-// 创建SVG容器
-const svg = d3.select("#chart")
-  .append("svg")
-  .attr("width", CHART_CONFIG.width)
-  .attr("height", CHART_CONFIG.height);
+// 创建SVG容器和比例尺
+function setupScales() {
+  // 初始化类别和权重
+  categories = Object.keys(data.entities[0].scores);
+  weights = categories.map(() => 1 / categories.length);
+  
+  // 计算分数极值
+  calculateScoreExtremes();
+  
+  // 创建SVG容器
+  svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", CHART_CONFIG.width)
+    .attr("height", CHART_CONFIG.height);
 
-// 创建比例尺
-const x = d3.scaleBand()
-  .domain(data.entities.map(d => d.name))
-  .range([CHART_CONFIG.margin.left, CHART_CONFIG.width - CHART_CONFIG.margin.right])
-  .padding(0.1);
+  // 创建比例尺
+  x = d3.scaleBand()
+    .domain(data.entities.map(d => d.name))
+    .range([CHART_CONFIG.margin.left, CHART_CONFIG.width - CHART_CONFIG.margin.right])
+    .padding(0.1);
 
-const y = d3.scaleLinear()
-  .domain([minScore, maxScore])
-  .range([CHART_CONFIG.height - CHART_CONFIG.margin.bottom, CHART_CONFIG.margin.top]);
+  y = d3.scaleLinear()
+    .domain([minScore, maxScore])
+    .range([CHART_CONFIG.height - CHART_CONFIG.margin.bottom, CHART_CONFIG.margin.top]);
 
-const petalLengthScale = d3.scaleLinear()
-  .domain([minScore, maxScore])
-  .range([0, 100]);
+  petalLengthScale = d3.scaleLinear()
+    .domain([minScore, maxScore])
+    .range([0, 100]);
 
-const color = d3.scaleOrdinal(d3.schemeCategory10);
+  color = d3.scaleOrdinal(d3.schemeCategory10);
+}
 
 // 绘制x轴
-svg.append("g")
-  .attr("transform", `translate(0,${CHART_CONFIG.height - CHART_CONFIG.margin.bottom})`)
-  .call(d3.axisBottom(x));
+function drawXAxis() {
+  svg.append("g")
+    .attr("transform", `translate(0,${CHART_CONFIG.height - CHART_CONFIG.margin.bottom})`)
+    .call(d3.axisBottom(x));
+}
 
 // 计算加权分数的辅助函数
 function calculateWeightedScores() {
@@ -234,11 +251,13 @@ function addChartCaption() {
 
 // 初始化函数
 function initializeChart() {
+  setupScales();
+  drawXAxis();
   setupGridLines();
   addChartCaption();
   setupSliders();
   updateChart();
 }
 
-// 启动应用
-initializeChart();
+// 启动应用 - 首先加载数据
+loadData();
